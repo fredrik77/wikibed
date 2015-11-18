d14f4a655d7c80216a612efcf3498dc87c471eb1
vrghost242 <bengtbj@gmail.com>
2015-11-18 17:34:10 +0000

    Update wikibed.js
    Working on the getArticle version to allow for splitting and stripping links, not quite done yet though

--- wikibed.js
+++ wikibed.js
@@ -15,10 +15,23 @@ angular.module('wikiBed', [])
         var wikiConnect = [];
         var deferred = $q.defer();
         var returnData = [];
-        wikiBedFactory.init = function (url){
-            wikiConnect.apiurl = url; // Send the api url to the wiki, for example 'http://thisismysite/wiki/api.php'
-            wikiConnect.format = 'json';
-            wikiConnect.action = 'parse';
+        wikiBedFactory.init = function (options){
+            wikiConnect.url = options.url || 'wiki' // Send the url to the wiki, for example 'http://thisismysite/wiki/' if site apicall is on 'http://thisismysite/wiki/api.php'
+            wikiConnect.apiurl = options.apiurl || wikiConnect.url+'/api.php' // Stuff back
+            wikiConnect.format = options.format || 'json' ; // Please note that changing format from json will force raw=true
+            if ( wikiConnect.format != 'json' ) {
+                wikiConnect.raw = true;
+            } else {
+                wikiConnect.raw = options.raw || false; // Set to true and you will get the raw feed from wikipedia back, please note that you get the whole data object back.
+            }
+            wikiConnect.splitToc = options.splitToc || true; //Split the article by TOC and return under data.byToc
+            wikiConnect.stripToc = options.stripToc || true; //Weather or not to remove the TOC reference, normally the heading of that section
+            wikiConnect.rewriteUrl = options.rewriteUrl || true; //Rewrite url for images and resources (not internal links though)
+            wikiConnect.rewriteRoot = options.rewriteRoot || '/wiki/' //How the actual wiki reefers to its internal files
+            wikiConnect.rewriteTarget = options.rewriteTarget || '/wikiproxy/'   // If set, any links for images for example /wiki/images goes to /wikiproxy/images
+            wikiConnect.rewriteIntLink = options.rewriteIntLink || true // Rewrite internal links other than resources
+            wikiConnect.rewriteIntTarget = options.rewriteIntTarget || '/wiki/article/' // In our example, we have a route in express called wiki. Remember that this route will need to be able to handle wiki calls such as wiki/index.php/FMP/Front_Office/Liquidity_Management
+            wikiConnect.rewriteIntRoot = options.rewriteIntRoot || '/wiki/index.php/' //How Wikipedia normally states its internal articles
         }
         wikiBedFactory.getArticle = function( article ){
             //returnData.url = wikiConnect.api + "?action=" + wikiConnect.action + "&page=" + article + "&format=" + wikiConnect.format; // Might come in handy
@@ -26,8 +39,19 @@ angular.module('wikiBed', [])
             $http({
                     url: wikiConnect.apiurl,
                     method: "GET",
-                    params: {'action': wikiConnect.action, 'format': wikiConnect.format, 'page': article}
+                    params: {'action': 'parse', 'format': wikiConnect.format, 'page': article}
                 }).then(function (data){
+                    if ( data.data.error ) {
+                        console.log("Got an unknown error from API when attempting to get:");
+                        console.log(wikiConnect.apiurl);
+                        console.log("{'action': "+ 'parse' + ", 'format': " + wikiConnect.format + ", 'page': "+  article +"}");
+                        deferred.reject(data.data.error);
+                        return;
+                    }
+                    if (wikiConnect.raw) {
+                        deferred.resolve(data.data)
+                        return;
+                    }
                     parse=data.data.parse
                     parse.url=article;
                     var star = "*";
@@ -36,6 +60,34 @@ angular.module('wikiBed', [])
                     for (var i in parse) {
                         console.log("ID: " + i + " DATA: " + parse[i]);
                     }
+                    if (wikiConnect.splitToc) {
+                        console.log ("I will split the TOC ");
+                        parse.splitToc = [];
+                        var tmpStr = parse.text[star].toString();
+
+                        for (var sectionId in parse.sections) {
+                            parse.splitToc[sectionId] = [];
+                            console.log("Sections ID: " + sectionId + " DATA: " + parse.sections[sectionId]);
+
+                            for (var i in parse.sections[sectionId]){
+                                console.log("Sections "+ [sectionId] +" ID: " + i + " DATA: " + parse.sections[sectionId][i]);
+
+                            }
+                            var nextsection = parseInt(sectionId) + 1;
+
+                            if (parse.sections[nextsection]['byteoffset']){
+                                parse.splitToc[sectionId].text = tmpStr.slice(parse.sections[sectionId]['byteoffset'],parse.sections[nextsection]['byteoffset'])
+                                console.log("New Section:");
+                                console.log(parse.splitToc[sectionId].text);
+                            } else {
+                                parse.splitToc[sectionId].text = tmpStr.slice(parse.sections[sectionId]['byteoffset'])
+                                console.log("New Section:");
+                                console.log(parse.splitToc[sectionId].text);
+                            }
+
+                        }
+                    }
+
                     //console.log ("Got some data " + data)
                     deferred.resolve(parse);
                 }
@@ -53,4 +105,3 @@ angular.module('wikiBed', [])
 
 // 5. define another module component
     .directive('directiveName', function() {/* stuff here */})
-;// and so on
